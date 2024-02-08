04014e964f5e5a35375af79d631ff9b4e6e9bb4e
gr0nn1x <fanyfany888@gmail.com>
2024-02-08 07:43:26 +0100

    added floor menu/ not working

--- App.js
+++ App.js
@@ -28,71 +28,81 @@ export default function App() {
   };
 
   return (
-    <ScrollView horizontal={true}>
-      <View style={styles.container}>
-        {dataList.map((data) => {
-          return (
-            <TouchableWithoutFeedback key={data.id} onPress={() => test(data)}>
-              <View
-                style={[
-                  {
-                    left: data.x,
-                    top: data.y,
-                    width: data.width,
-                    height: data.height,
-                    borderColor: data.color,
-                  },
-                  styles.room,
-                ]}
-              >
-                <Text>{data.name}</Text>
-              </View>
-            </TouchableWithoutFeedback>
-          );
-        })}
-        <TouchableWithoutFeedback onPress={() => setIsModalVisible(false)}>
-          <Modal
-            visible={isModalVisible}
-            onRequestClose={closeModal}
-            animationType="slide"
-            presentationStyle="pageSheet"
-          >
-            <View style={styles.modalContainer}>
-              <View style={styles.modalContent}>
-                <Text style={styles.modalText}>
-                  {selectedData ? selectedData.name : ""}
+    <View style={styles.container}>
+      <ScrollView>
+        <ScrollView horizontal={true}>
+          <View style={styles.content}>
+            {dataList.map((data) => {
+              return (
+                <TouchableWithoutFeedback
+                  key={data.id}
+                  onPress={() => test(data)}
+                >
+                  <View
+                    style={[
+                      {
+                        left: data.x,
+                        top: data.y,
+                        width: data.width,
+                        height: data.height,
+                        borderColor: data.color,
+                      },
+                      styles.room,
+                    ]}
+                  >
+                    <Text>{data.name}</Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              );
+            })}
+          </View>
+        </ScrollView>
+        <Modal
+          visible={isModalVisible}
+          onRequestClose={closeModal}
+          animationType="slide"
+          presentationStyle="pageSheet"
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalText}>
+                {selectedData ? selectedData.name : ""}
+              </Text>
+              {selectedData && selectedData.teachers && (
+                <Text style={styles.teacherText}>
+                  Teacher: {selectedData.teachers}
                 </Text>
-                {selectedData && selectedData.teachers && (
-                  <Text style={styles.teacherText}>
-                    Teacher: {selectedData.teachers}
-                  </Text>
-                )}
-                <View style={styles.buttonContainer}>
-                  <Button title="Close" onPress={closeModal} />
-                </View>
+              )}
+              <View style={styles.buttonContainer}>
+                <Button title="Close" onPress={closeModal} />
               </View>
             </View>
-          </Modal>
-        </TouchableWithoutFeedback>
+          </View>
+        </Modal>
+      </ScrollView>
+      <View style={styles.fixedView}>
         <View style={[styles.switch, styles.switchOne]}>
-          <Text style={styles.switchText}>1</Text>
+          <Text style={styles.switchText}>Třetí Patro</Text>
         </View>
         <View style={[styles.switch, styles.switchTwo]}>
-          <Text style={styles.switchText}>2</Text>
+          <Text style={styles.switchText}>Druhé Patro</Text>
         </View>
         <View style={[styles.switch, styles.switchThree]}>
-          <Text style={styles.switchText}>3</Text>
+          <Text style={styles.switchText}>Přízemí</Text>
         </View>
         <View style={[styles.switch, styles.switchFour]}>
-          <Text style={styles.switchText}>4</Text>
+          <Text style={styles.switchText}>Nulté Patro</Text>
         </View>
       </View>
-    </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  content: {
     width: 700,
     height: 1000,
     backgroundColor: "#DCF2F1",
@@ -131,27 +141,25 @@ const styles = StyleSheet.create({
   buttonContainer: {
     marginTop: "auto",
   },
+  fixedView: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "space-between",
+    padding: 10,
+  },
   switch: {
-    height: 100,
-    width: 100,
+    height: windowHeight * 0.06,
+    width: windowWidth * 0.95,
+    marginTop: 10,
     backgroundColor: "black",
-    borderRadius: 10,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  switchText: { color: "white", fontSize: 20 },
-  switchOne: {
-    top: windowWidth * 1.8,
-    left: windowHeight * 0.05,
-  },
-  switchTwo: {
-    top: windowWidth * 1.558,
-    left: windowHeight * 0.2,
-  },
-  switchThree: {
-    top: windowWidth * 1.316,
-    left: windowHeight * 0.35,
-  },
-  switchFour: {
-    top: windowWidth * 1.072,
-    left: windowHeight * 0.5,
+  switchText: {
+    color: "white",
+    fontSize: 20,
   },
 });
